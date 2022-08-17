759177061065a109276e547809734e958d39f753
M_NIHAD <nigad779@gmail.com>
2022-08-17 11:08:52 +0530

    firstcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 // import Preview from './widgets/Preview'
 // import RatingComp from './widgets/interactive rating compound/RatingComp'
-// import NFT from './widgets/NFT Preview card/NFT'
-import SignUp from './widgets/Sign Up Page/SignUp'
+import NFT from './widgets/NFT Preview card/NFT'
+// import SignUp from './widgets/Sign Up Page/SignUp'
 const App = () => {
   return (
     // <Preview/>
     // <RatingComp/>
-    // <NFT/>
-    <SignUp/>
+    <NFT/>
+    // <SignUp/>
 
   )
 }
